b1189b6e338d849987eb9d3996a4ccc15134f26e
leokan <leo.kan01@gmail.com>
2022-03-08 11:00:01 +0100

    update

---
+++ hw/02/cpp/cmake-build-release/CMakeFiles/cjdemo.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/leokan/Documents/TUDelft/GEO1004-2022/3d-hw/hw2/geo1004.2022_hw02/hw/02/cpp/src/main.cpp" "CMakeFiles/cjdemo.dir/src/main.cpp.o" "gcc" "CMakeFiles/cjdemo.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
