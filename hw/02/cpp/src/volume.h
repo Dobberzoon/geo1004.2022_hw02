60ebc8b97b72a3c2c10a4c1bcd17d5db0263a859
Dobberzoon <danieldobsonmail@gmail.com>
2022-03-09 18:20:11 +0100

    added determinant function found from internet, it works. TODO: test on imported tetrahedron

--- hw/02/cpp/src/volume.h
+++ hw/02/cpp/src/volume.h
@@ -4,5 +4,9 @@
 
 #ifndef CJDEMO_VOLUME_H
 #define CJDEMO_VOLUME_H
+#define N 3
+
+void subMatrix(int mat[N][N], int temp[N][N], int p, int q, int n);
+int determinantOfMatrix(int matrix[N][N], int n);
 
 #endif //CJDEMO_VOLUME_H
