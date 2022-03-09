d7766df1b0a59eed8922dc411567fd9b3ba6d486
leokan <leo.kan01@gmail.com>
2022-03-09 13:01:22 +0100

    update

--- hw/02/cpp/src/main.cpp
+++ hw/02/cpp/src/main.cpp
@@ -44,56 +44,54 @@
 
 using json = nlohmann::json;
 
-
-
 int   get_no_roof_surfaces(json &j);
 void  list_all_vertices(json& j);
 void  visit_roofsurfaces(json &j);
 
 
-
-
 int main(int argc, const char * argv[]) {
 
-  //-- reading the file with nlohmann json: https://github.com/nlohmann/json  
-  std::ifstream input("../../data/twobuildings.city.json");
-  json j;
-  input >> j;
-  input.close();
+    //-- reading the file with nlohmann json: https://github.com/nlohmann/json
+    std::ifstream input("../../data/twobuildings.city.json");
+    json j; // empty json object
+    input >> j;  // std::maps & std::array / std::vector
+    input.close();
 
-  //-- get total number of RoofSurface in the file
-  // int noroofsurfaces = get_no_roof_surfaces(j);
-  // std::cout << "Total RoofSurface: " << noroofsurfaces << std::endl;
+    //  -- get total number of RoofSurface in the file
+    int noroofsurfaces = get_no_roof_surfaces(j);
+    std::cout << "Total RoofSurface: " << noroofsurfaces << std::endl;
 
-  // list_all_vertices(j);
+    std::cout << "list all vertices" << std::endl;
+    list_all_vertices(j);
 
-  visit_roofsurfaces(j);
+    visit_roofsurfaces(j);
 
-  //-- print out the number of Buildings in the file
-  int nobuildings = 0;
-  for (auto& co : j["CityObjects"]) {
-    if (co["type"] == "Building") {
-      nobuildings += 1;
+    //-- print out the number of Buildings in the file
+    int nobuildings = 0;
+    for (auto& co : j["CityObjects"]) {
+        if (co["type"] == "Building") {
+          nobuildings += 1;
+        }
     }
-  }
-  std::cout << "There are " << nobuildings << " Buildings in the file" << std::endl;
+    std::cout << "There are " << nobuildings << " Buildings in the file" << std::endl;
 
-  //-- print out the number of vertices in the file
-  std::cout << "Number of vertices " << j["vertices"].size() << std::endl;
+    //-- print out the number of vertices in the file
+    std::cout << "Number of vertices " << j["vertices"].size() << std::endl;
 
-  //-- add an attribute "volume"
-  for (auto& co : j["CityObjects"]) {
-    if (co["type"] == "Building") {
-      co["attributes"]["volume"] = rand();
+    //-- add an attribute "volume"
+    for (auto& co : j["CityObjects"]) {
+        if (co["type"] == "Building") {
+          co["attributes"]["volume"] = 8964;
+        }
     }
-  }
 
-  //-- write to disk the modified city model (myfile.city.json)
-  std::ofstream o("myfile.city.json");
-  o << j.dump(2) << std::endl;
-  o.close();
 
-  return 0;
+    //-- write to disk the modified city model (myfile.city.json)
+    std::ofstream o("area.city.json");
+    o << j.dump(2) << std::endl;
+    o.close();
+
+    return 0;
 }
 
 
@@ -140,25 +138,25 @@ int get_no_roof_surfaces(json &j) {
 // CityJSON files have their vertices compressed: https://www.cityjson.org/specs/1.1.1/#transform-object
 // this function visits all the surfaces and print the (x,y,z) coordinates of each vertex encountered
 void list_all_vertices(json& j) {
-  for (auto& co : j["CityObjects"].items()) {
-    std::cout << "= CityObject: " << co.key() << std::endl;
-    for (auto& g : co.value()["geometry"]) {
-      if (g["type"] == "Solid") {
-        for (auto& shell : g["boundaries"]) {
-          for (auto& surface : shell) {
-            for (auto& ring : surface) {
-              std::cout << "---" << std::endl;
-              for (auto& v : ring) { 
-                std::vector<int> vi = j["vertices"][v.get<int>()];
-                double x = (vi[0] * j["transform"]["scale"][0].get<double>()) + j["transform"]["translate"][0].get<double>();
-                double y = (vi[1] * j["transform"]["scale"][1].get<double>()) + j["transform"]["translate"][1].get<double>();
-                double z = (vi[2] * j["transform"]["scale"][2].get<double>()) + j["transform"]["translate"][2].get<double>();
-                std::cout << std::setprecision(2) << std::fixed << v << " (" << x << ", " << y << ", " << z << ")" << std::endl;                
-              }
+    for (auto& co : j["CityObjects"].items()) {
+        std::cout << "= CityObject: " << co.key() << std::endl;
+        for (auto& g : co.value()["geometry"]) {
+            if (g["type"] == "Solid") {
+                for (auto& shell : g["boundaries"]) {
+                    for (auto& surface : shell) {
+                        for (auto& ring : surface) {
+                            std::cout << "---" << std::endl;
+                            for (auto& v : ring) {
+                                std::vector<int> vi = j["vertices"][v.get<int>()];
+                                double x = (vi[0] * j["transform"]["scale"][0].get<double>()) + j["transform"]["translate"][0].get<double>();
+                                double y = (vi[1] * j["transform"]["scale"][1].get<double>()) + j["transform"]["translate"][1].get<double>();
+                                double z = (vi[2] * j["transform"]["scale"][2].get<double>()) + j["transform"]["translate"][2].get<double>();
+                                std::cout << std::setprecision(2) << std::fixed << v << " (" << x << ", " << y << ", " << z << ")" << std::endl;
+                            }
+                        }
+                    }
+                }
             }
-          }
         }
-      }
     }
-  }
 }
