c9dee61defd9950cc66449c391586de1e83071f4
AIGheorg <92847408+AIGheorg@users.noreply.github.com>
2022-03-14 15:15:33 +0100

    Update main.cpp
    - added the functions for distance and heron's area
    - made a prototype of the roof_area, still needs a lot of corrections

--- hw/02/cpp/src/main.cpp
+++ hw/02/cpp/src/main.cpp
@@ -32,24 +32,47 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 #include "json.hpp"
 
 using json = nlohmann::json;
 
 
+double ax,ay,bx,by;
+std::vector<std::vector<double>>vic;
 
 int   get_no_roof_surfaces(json &j);
 void  list_all_vertices(json& j);
 void  visit_roofsurfaces(json &j);
 void no_of_floors (json &j);
+void vertices_roof(json &j);
+void  list_all_vertices01(json& j);
 
+float distance(double ax,double ay,double bx,double by)
+{
+float dist=sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by));
+return dist;
+}
 
+float heron(std::vector<std::vector<double>>vic){
+    float sp;
+    float a, b, c;
+    float area;
+    a=distance(vic[0][0],vic[0][1],vic[1][0],vic[1][1]);
+    b=distance(vic[1][0],vic[1][1],vic[2][0],vic[2][1]);
+    c=distance(vic[2][0],vic[2][1],vic[0][0],vic[0][1]);
+
+    sp=(a+b+c)/2;
+
+    area=sqrt(sp*(sp-a)*(sp-b)*(sp-c));
+    return area;
+}
 
 
 int main(int argc, const char * argv[]) {
 
   //-- reading the file with nlohmann json: https://github.com/nlohmann/json  
-  std::ifstream input("../../data/twobuildings.city.json");
+  std::ifstream input("../../data/twob1.json");
   json j;
   input >> j;
   input.close();
@@ -58,10 +81,14 @@ int main(int argc, const char * argv[]) {
   // int noroofsurfaces = get_no_roof_surfaces(j);
   // std::cout << "Total RoofSurface: " << noroofsurfaces << std::endl;
 
-  // list_all_vertices(j);
+   //list_all_vertices(j);
 
-  visit_roofsurfaces(j);
-  no_of_floors(j);
+  //visit_roofsurfaces(j);
+
+  //no_of_floors(j);
+    //vertices_roof;
+    list_all_vertices01(j);
+
 
   //-- print out the number of Buildings in the file
   int nobuildings = 0;
@@ -81,6 +108,12 @@ int main(int argc, const char * argv[]) {
       co["attributes"]["volume"] = rand();
     }
   }
+
+  /*for (auto& co : j["CityObjects"]) {
+      if (co["type"] == "Building") {
+            co["attributes"]["no. of floors"] = no_of_floors(co);
+      }
+  }*/
 
   //-- write to disk the modified city model (myfile.city.json)
   std::ofstream o("myfile.city.json");
@@ -170,8 +203,92 @@ float rdif;
             hmin=co["attributes"]["h_dak_min"];
             hgrlvl=co["attributes"]["h_maaiveld"];
             rdif=((hmax-hmin)*0.7+(hmin-hgrlvl))/3.0;
-            std::cout<<int(rdif)<<"\n";
+            std::cout<<round(rdif)<<"\n";
 
             }
         }
     }
+
+
+/*float distance(double ax,double ay,double bx,double by);
+{
+  float dist=sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by));
+  return dist;
+}*/
+
+void vertices_roof(json& j) {
+    /*for (auto &co: j["CityObjects"].items()) {
+        for (auto &g: co.value()["geometry"]) {
+            for (auto &shell0: g["semantics"]["values"]){
+                for (auto &s: shell0){
+                    if (g["semantics"]["surfaces"][s.get<int>()]["type"].get<std::string>().compare("RoofSurface") == 0){
+                        for(auto &shell1: g["boundaries"])
+
+                    }
+                }
+            }}
+        }*/
+        }
+
+void list_all_vertices01(json& j) {
+
+    float ar, roof_area=0.00;
+    std::vector<std::vector<double>> triangle;
+    for (auto &co: j["CityObjects"].items()) {
+        roof_area=0.00;
+        std::cout << "= CityObject: " << co.key() << std::endl;
+        for (auto &g: co.value()["geometry"]) {
+            if (g["type"] == "Solid") {
+                for (auto &shell: g["boundaries"]) {
+                    for (auto &surface: shell) {
+                        //std::vector<std::vector<double>> triangle;
+                        for (auto &ring: surface) {
+                            //std::cout << "---" << std::endl;
+                            for (auto &v: ring) {
+                                std::vector<double>coord;
+                                std::vector<int> vi = j["vertices"][v.get<int>()];
+                                double x = (vi[0] * j["transform"]["scale"][0].get<double>()) +
+                                           j["transform"]["translate"][0].get<double>();
+                                double y = (vi[1] * j["transform"]["scale"][1].get<double>()) +
+                                           j["transform"]["translate"][1].get<double>();
+                                //double z = (vi[2] * j["transform"]["scale"][2].get<double>()) + j["transform"]["translate"][2].get<double>();
+                                //std::cout<< v << " (" << x << ", " << y << ", " << z << ")" << std::endl;
+                                coord.emplace_back(x);
+                                coord.emplace_back(y);
+                                triangle.emplace_back(coord);}
+
+                            ar=heron(triangle);
+                            roof_area=ar+roof_area;
+
+
+
+
+                        }
+                        //std::cout<<roof_area<<std::endl;
+
+                            }
+                        }
+                    }
+                }
+            //std::cout<<"Area of roof "<<roof_area<<std::endl;
+
+        std::cout<<"Area of roof "<<roof_area<<std::endl;}
+    //std::cout<<"Area of roof "<<roof_area<<std::endl;
+}
+
+
+/*for(auto i:triangle)
+{std::cout<<i[0]<<" "<<i[1]<<std::endl;}*/
+/*float heron(std::vector<std::vector<double>>vic1){
+    float sp;
+    float a, b, c;
+    float area;
+    a=distance(vic1[0][0],vic1[0][1],vic1[1][0],vic1[1][1]);
+    b=distance(vic1[1][0],vic1[1][1],vic1[2][0],vic1[2][1]);
+    c=distance(vic1[2][0],vic1[2][1],vic1[0][0],vic1[0][1]);
+
+    sp=(a+b+c)/2;
+
+    area=sqrt(sp*(sp-a)*(sp-b)*(sp-c));
+    return area;
+}*/
