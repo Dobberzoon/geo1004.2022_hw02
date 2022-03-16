266426894ab853781fa259544e01467d157a6f73
Dobberzoon <danieldobsonmail@gmail.com>
2022-03-16 22:16:31 +0100

    add some cross product and new area function, to improve on naive heron method

--- hw/02/cpp/src/newattributes.h
+++ hw/02/cpp/src/newattributes.h
@@ -17,9 +17,13 @@ double determinantOfMatrix(double (&matrix)[N][N], int n);
 void readObj(std::string &file_in, std::vector<std::vector<double>> &vertices, std::vector<std::vector<int>> &face_indices);
 void fillMatrix4x4(double (&mat)[N][N], std::vector<std::vector<double>> &vertices, int n);
 double volumeTetra(double &determinant);
-double volumeObject(std::vector<double> &outside_point, std::vector<std::vector<double>> &vertices,
-                    std::vector<std::vector<int>> &face_indices);
 void getCOVolumes(json &j);
 void getCOBuildingHeights(json &j);
+void crossProduct(int vect_A[3], int vect_B[3], int cross_P[3]);
+double areaTriangle(int cross_P[3]);
+
+// REMOVE OLD CODE BEFORE SUBMISSION
+//double volumeObject(std::vector<double> &outside_point, std::vector<std::vector<double>> &vertices,
+//                    std::vector<std::vector<int>> &face_indices);
 
 #endif //CJDEMO_VOLUME_H
